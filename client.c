14a4141a0d7ad7fda3dde265954ba6beac80519a
Marie BRIAND <mabriand@e1r10p23.42.fr>
2021-12-16 15:16:59 +0100

    need to check: sigusr1 & 2, bitshifting

--- client.c
+++ client.c
@@ -5,34 +5,20 @@
 #include <stdlib.h>
 
 #include <stdio.h>
-
-// struct sigaction
-// {
-//     void     (*sa_handler) (int);
-//     void     (*sa_sigaction) (int, siginfo_t *, void *);
-//     sigset_t   sa_mask;
-//     int        sa_flags;
-//     void     (*sa_restorer) (void);
-// };
  
-void    printMsgHandler(int signum)
-{
-    printf("YOLO I MADE IT\n");
-    return ;
-}
 int     main(int ac, char **av)
 {
     if (ac != 3)
+    {
+        printf("Error: usage is as follows:\n './client [PID] [string]'\n");
         return (0);
+    }
     printf("Client speaking, I am receptionning PID = %s from server\n", av[1]);
-    printf("I must send him the following string: %s\n", av[2]);
+    printf("I must send him the following string: %s\n\n", av[2]);
 
     int pid = atoi(av[1]);
+    printf("pid receptionned: %d\n", pid);
 
-    struct sigaction new_action, old_action;
-
-    ret = sigaction(31, )
-    // kill(pid, SIGTERM);
-
+    kill(pid, SIGUSR1);
     return (0);
 }
--- server.c
+++ server.c
@@ -1,21 +1,39 @@
 #include <unistd.h>
 #include <sys/types.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <stdlib.h>
 
 #include <stdio.h>
 
 // Il faudrait regarder `nouveau les fonctions autoorrisées
+
+void    printMsgHandler(int signum, siginfo_t *info, void *unused)
+{
+    (void)unused;
+    (void)info;
+    printf("YOLO I MADE IT\n");
+    printf("signum: %d\n", signum);
+    return ;
+}
 
 int main()
 {
     pid_t   pid_server;
     pid_server = getpid();
 
-    printf("Hi I'm server and this is my PID : %d\n", pid_server);
+    printf("Hi I'm server and this is my PID : %d\n\n", pid_server);
 
+    struct sigaction	s;
+
+	s.sa_sigaction = printMsgHandler;
+    s.sa_flags = SA_SIGINFO;
+
+    sigaction(SIGUSR1, &s, NULL);
     pause();
     
 
     return (0);
 
 
-}
+} 
